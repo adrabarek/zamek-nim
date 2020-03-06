9008f71ec7e04798b80e88189e7262502b015572
Andrzej <andrzej.drabarek@protonmail.com>
2020-03-06 22:32:03 +0100

    Cleaned up and finished CLI processing.

--- zamek_cli.nim
+++ zamek_cli.nim
@@ -1,35 +1,70 @@
 import os
 import parseopt
 
+type
+  Command = enum
+    none, create, add, remove, edit, tag_add, tag_remove, find, find_by_tags, find_connected
+  Setting = enum
+    verbose
+  Settings = set[Setting]
+
 proc handleInvalidParams() =
-  echo "Usage: zamek COMMAND TARGET..."
+  echo """usage: zamek [--verbose] [--help] <command> [<args>]
+
+Commands:
+  create
+  add
+  remove
+  edit
+  tag-add
+  tag-remove
+  find
+  find-by-tags
+  find-connected"""
   quit(QuitFailure)
 
-if paramCount() < 1:
-  handleInvalidParams()
+proc processCommandLine() : (Command, Settings) =
+  var args = initOptParser(commandLineParams())  
 
-var args = initOptParser(commandLineParams())
+  var command = Command.none
+  var settings: Settings
 
-args.next()
-if args.kind != cmdArgument:
-  handleInvalidParams()
+  for kind, key, val in args.getopt():
+    case kind
+    of cmdArgument:
+      case key
+      of "create":
+        command = Command.create
+      of "add":
+        command = Command.add
+      of "remove":
+        command = Command.remove
+      of "edit":
+        command = Command.edit
+      of "tag-add":
+        command = Command.tag_add
+      of "tag-remove":
+        command = Command.tag_remove
+      of "find":
+        command = Command.find
+      of "find-by-tags":
+        command = Command.find_by_tags
+      of "find-connected":
+        command = Command.find_connected
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "verbose", "v":
+        incl(settings, Setting.verbose)
+    else:
+      handleInvalidParams()
 
-case args.key
-of "create":
-  echo "Creating a new notes repository."
-of "add":
-  echo "Adding new note."
-of "remove":
-  echo "Removing a note."
-of "edit":
-  echo "Editing a note."
-of "tag-add":
-  echo "Adding a tag to a note."
-of "tag-remove":
-  echo "Removing a tag from a note."
-of "find":
-  echo "Finding notes by content search."
-of "find-by-tags":
-  echo "Finding notes by tags."
-of "find-connected":
-  echo "Findinc connected notes."
+  if command == Command.none:
+    handleInvalidParams()
+
+  (command, settings)
+
+let (command, settings) = processCommandLine()
+
+if Setting.verbose in settings:
+  echo "Verbose mode on!"
+  echo "Running command: ", command
