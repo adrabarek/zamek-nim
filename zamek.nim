0b23e109239fbcc9456c2e09f5d8ef1aeb25875a
Andrzej <andrzej.drabarek@protonmail.com>
2020-03-14 13:25:54 +0100

    Refactor.

--- zamek.nim
+++ zamek.nim
@@ -1,9 +1,8 @@
-import tables
-import sets
+import os, tables, sets, marshal, times
 
-const dirName* = ".zamek"
-const regFileName* = "registry"
-const noteExtension* = ".znot"
+const dirName = ".zamek"
+const registryFileName = "registry"
+const noteExtension = ".znot"
 
 type
   SettingFlag* = enum
@@ -16,18 +15,101 @@ type
     content*: string
     tags*: seq[Tag]
     links*: seq[NoteName]
-  Registry* = object
+  Registry = object
     links : Table[NoteName, HashSet[NoteName]]
     tags : Table[Tag, HashSet[NoteName]]
+  Paths = object
+    zamekDir: string
+    registryFilePath: string
 
+proc createPaths(root: string): Paths =
+  let zamekDir = joinPath(root, zamek.dirName)
+  let registryFilePath = joinPath(zamekDir, zamek.registryFileName)
+  Paths(zamekDir: zamekDir, registryFilePath: registryFilePath)
 
-proc addNote*(registry: var Registry, note: Note) =
-    for tag in note.tags:
-      if tag notin registry.tags:
-        registry.tags[tag] = HashSet[NoteName]()
-      registry.tags[tag].incl(note.name)
+proc backupFile(path: string, maxBackups: int): bool =
+    var nTries = 0
+    while nTries < maxBackups:
+      let backupPath = path & "_backup" & $(now().format("yyyy-MM-dd")) & "_" & $(nTries)
+      if not fileExists(backupPath):
+        moveFile(path, backupPath)
+        break
+      inc nTries
+    if(nTries == maxBackups):
+      return false
+    return true
 
-    for otherNote in note.links:
-      if note.name notin registry.links:
-        registry.links[note.name] = HashSet[NoteName]()
-      registry.links[note.name].incl(otherNote)
+proc onlyZamekFilesPresent(path: string): bool = 
+  for path in walkPattern(path & "/*"):
+    var (_, _, ext) = splitFile(path)
+    if ext != zamek.noteExtension:
+      return false
+  return true
+
+proc isZamekDirectory(path: string): bool =
+  dirExists(joinPath(path, zamek.dirName)) and onlyZamekFilesPresent(path)
+
+proc addNote(registry: var Registry, note: Note): bool =
+  for tag in note.tags:
+    if tag notin registry.tags:
+      registry.tags[tag] = HashSet[NoteName]()
+    registry.tags[tag].incl(note.name)
+
+  for otherNote in note.links:
+    if note.name notin registry.links:
+      registry.links[note.name] = HashSet[NoteName]()
+    registry.links[note.name].incl(otherNote)
+
+proc addNote*(root: string, note: Note): bool =
+    if not isZamekDirectory(root):
+      echo "Not a valid Zamek directory. Initialize one using create command."
+      return false
+
+    let notePath = joinPath(root, note.name & zamek.noteExtension)
+
+    if fileExists(notePath):
+      echo "Cannot add note - note with that name already exists."
+      return false
+
+    writeFile(notePath, $$(note))
+    setFilePermissions(notePath, {fpUserRead, fpGroupRead, fpOthersRead})
+
+    let paths = createPaths(root)
+    var registry = to[zamek.Registry](readFile(paths.registryFilePath))
+    if not addNote(registry, note):
+      echo "Failed to add note to the registry."
+      return false
+    writeFile(paths.registryFilePath, $$(registry))
+
+proc createRepository*(root: string): bool =
+  let paths = createPaths(root)
+
+  if not onlyZamekFilesPresent(root):
+    echo "Zamek repository needs to be created in an empty directory or a previous Zamek repository."
+    return false
+
+  # create zamek control directory if doesn't exist yet
+  if not dirExists(paths.zamekDir):
+    createDir(paths.zamekDir)
+
+  var registry: zamek.Registry
+  for file in walkPattern(getCurrentDir() & "/*"):
+    # add note to the registry
+    var note = to[Note](readFile(file))
+    if not addNote(registry, note):
+      echo "Failed to add present note to new repository. Aborting."
+      return false
+    # make the note file read-only
+    setFilePermissions(file, {fpUserRead, fpGroupRead, fpOthersRead})
+
+  # backup current zamek registry file if already exists
+  if fileExists(paths.registryFilePath):
+    const maxBackups = 10
+    if not backupFile(paths.registryFilePath, maxBackups):
+      echo "Failed to backup registry file " & paths.registryFilePath
+      echo "Remove some of the backups under " & paths.zamekDir & " and try again."
+      return false
+
+  # save the registry file
+  writeFile(paths.registryFilePath, $$registry)
+  return true;
--- zamek_cli.nim
+++ zamek_cli.nim
@@ -1,13 +1,10 @@
-import os, parseopt, times, marshal, strutils
+import os, parseopt, strutils
 import zamek
 
 type
   Command = enum
     none, create, add, remove, edit, tag_add, tag_remove, find, find_by_tags, find_connected
   Arguments = seq[string]
-  Paths = object
-    zamekDir: string
-    registryFilePath: string
 
 proc handleInvalidParams() =
   echo """usage: zamek [--verbose] [--help] <command> [<args>]
@@ -72,70 +69,14 @@ proc processCommandLine() : (Command, Arguments, zamek.Settings) =
 
   (command, arguments, settings)
 
-proc onlyZamekFilesPresent(path: string): bool = 
-  for path in walkPattern(path & "/*"):
-    var (_, _, ext) = splitFile(path)
-    if ext != zamek.noteExtension:
-      return false
-  return true
-
-proc isZamekDirectory(path: string): bool =
-  dirExists(joinPath(path, zamek.dirName)) and onlyZamekFilesPresent(path)
-
-proc backupFile(path: string, maxBackups: int): bool =
-    var nTries = 0
-    while nTries < maxBackups:
-      let backupPath = path & "_backup" & $(now().format("yyyy-MM-dd")) & "_" & $(nTries)
-      if not fileExists(backupPath):
-        moveFile(path, backupPath)
-        break
-      inc nTries
-    if(nTries == maxBackups):
-      return false
-    return true
-
-proc doCreate(paths: Paths) =
-  # verify if this is a valid place for starting a Zamek repository
-  if not onlyZamekFilesPresent(getCurrentDir()):
-    echo "Zamek repository needs to be created in an empty directory or a previous Zamek repository."
-    quit(QuitFailure)
-
-  # create zamek control directory if doesn't exist yet
-  if not dirExists(paths.zamekDir):
-    createDir(paths.zamekDir)
-
-  # create the registry
-  var registry: zamek.Registry
-  for file in walkPattern(getCurrentDir() & "/*"):
-    # add note to the registry
-    var note = to[Note](readFile(file))
-    registry.addNote(note)
-    # make the note file read-only
-    setFilePermissions(file, {fpUserRead, fpGroupRead, fpOthersRead})
-
-  # backup current zamek registry file if already exists
-  if fileExists(paths.registryFilePath):
-    const maxBackups = 10
-    if not backupFile(paths.registryFilePath, maxBackups):
-      echo "Failed to backup registry file " & paths.registryFilePath
-      echo "Remove some of the backups under " & paths.zamekDir & " and try again."
-      quit(QuitFailure)
-
-  # save the registry file
-  writeFile(paths.registryFilePath, $$registry)
-
 proc toSnakeCase(s: string): string =
   toLowerAscii(strip(s)).replace(" ", "_")
 
-proc doAdd(paths: Paths, arguments: Arguments) =
+proc doAdd(arguments: Arguments) =
   # validate arguments
   if len(arguments) != 4 and len(arguments) != 3:
     echo "Wrong number of arguments for add command."
     handleInvalidParams()
-
-  if not isZamekDirectory(getCurrentDir()):
-    echo "Not a valid Zamek directory. Initialize one using create command."
-    quit(QuitFailure)
 
   var note: Note
   note.name = toSnakeCase(arguments[0])
@@ -155,21 +96,9 @@ proc doAdd(paths: Paths, arguments: Arguments) =
     # content comes from stdin
     note.content = readAll(stdin)
 
-  let notePath = joinPath(getCurrentDir(), note.name & zamek.noteExtension)
-  if fileExists(notePath):
-    echo "Cannot add note - note with that name already exists."
+  if not zamek.addNote(getCurrentDir(), note):
     quit(QuitFailure)
-  writeFile(notePath, $$(note))
-  setFilePermissions(notePath, {fpUserRead, fpGroupRead, fpOthersRead})
 
-  var registry = to[zamek.Registry](readFile(paths.registryFilePath))
-  registry.addNote(note)
-  writeFile(paths.registryFilePath, $$(registry))
-
-proc createPaths(): Paths =
-  let zamekDir = joinPath(getCurrentDir(), zamek.dirName)
-  let registryFilePath = joinPath(zamekDir, zamek.regFileName)
-  Paths(zamekDir: zamekDir, registryFilePath: registryFilePath)
 
 proc main() =
   let (command, arguments, settings) = processCommandLine()
@@ -178,15 +107,15 @@ proc main() =
     echo "Verbose mode on!"
     echo "Running command: ", command
 
-  let paths = createPaths()
-
   case command
   of none:
     assert(false, "If no command is set, should exit early - nothing to do.")
   of create:
-    doCreate(paths)
+    if not zamek.createRepository(getCurrentDir()):
+      echo "Failed to create Zamek repository."
+      quit(QuitFailure)
   of add:
-    doAdd(paths, arguments)
+    doAdd(arguments)
   of remove:
     discard
   of edit:
